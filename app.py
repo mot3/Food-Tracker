c6be4605bb90c3f2b9ff6d6877c44eadc5be721e
mot3 <hopdrmot3@gmail.com>
2021-04-30 07:48:15 +0430

    Add date and fix something

--- app.py
+++ app.py
@@ -1,34 +1,48 @@
-from flask import Flask, render_template, g, Request  
+from flask import Flask, render_template, g, request
 import sqlite3
-
-from flask.globals import request
+from datetime import datetime
 
 app = Flask(__name__)
+
 
 def connect_db():
     sql = sqlite3.connect('./data/food_log.db')
     sql.row_factory = sqlite3.Row
     return sql
 
+
 def get_db():
     if not hasattr(g, 'sqlite3_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
 @app.teardown_appcontext
 def close_db(error):
-    if hasattr(g, 'sqlite_db'): 
+    if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    db = get_db()
+
+    if request.method == 'POST':
+        date = request.form['date'] #assuming the date is in YYYY/MM/DD format
+        dt = datetime.strptime(date, '%Y-%m-%d')
+        database_date = datetime.strftime(dt, '%Y%m%d')
+
+        db.execute('INSERT INTO log_date (entry_date) values (?)', [database_date])
+
+
     return render_template('home.html')
+
 
 @app.route('/view')
 def view():
     return render_template('day.html')
 
-@app.route('/food', methods=['GET','POST'])
+
+@app.route('/food', methods=['GET', 'POST'])
 def food():
     db = get_db()
 
@@ -40,15 +54,17 @@ def food():
 
         calories = protein * 4 + carbohydrates * 4 + fat * 9
 
-        db.execute('insert into food (name, protein, carbohydrates, fat, calories) values (?,?,?,?,?)', \
-           [name, protein, carbohydrates, fat, calories])
+        db.execute('insert into food (name, protein, carbohydrates, fat, calories) values (?,?,?,?,?)',
+                   [name, protein, carbohydrates, fat, calories])
 
         db.commit()
 
-    cur = db.execute('SELECT name, protein, carbohydrates, fat, calories FROM food')
+    cur = db.execute(
+        'SELECT name, protein, carbohydrates, fat, calories FROM food')
     results = cur.fetchall()
 
-    return render_template('add_food.html', results = results)
+    return render_template('add_food.html', results=results)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
